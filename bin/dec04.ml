f829bbea1223c9ae3d3ca3491facb3aa4a56314c
Nicholas Schwab <git@nicholas-schwab.de>
2024-12-04 19:16:07 +0100

    Solve day 4.

---
+++ bin/dec04.ml
@@ -0,0 +1,63 @@
+open Aoc24
+
+let xmas = [ 'X'; 'M'; 'A'; 'S' ]
+
+type direction = { dx : int; dy : int }
+type position = { x : int; y : int }
+
+let pos_from_pair (x, y) = { x; y }
+
+let all_directions () =
+  Enum.cartesian_product (Enum.range (-1) ~until:1) (Enum.range (-1) ~until:1)
+  |> Enum.filter_map (fun (x, y) ->
+         if x == 0 && y == 0 then None else Some { dx = x; dy = y })
+
+let diagonals = [ { dx = 1; dy = 1 }; { dx = -1; dy = 1 } ]
+let reverse { dx; dy } = { dx = 0 - dx; dy = 0 - dy }
+let go { x; y } { dx; dy } = { x = x + dx; y = y + dy }
+
+let get_2d array pos =
+  Array.get_opt array pos.y |> fun o ->
+  Option.bind o (fun s -> String.get_opt s pos.x)
+
+let rec check_xmas field position direction = function
+  | [] -> true
+  | hd :: tl ->
+      if Some hd <> get_2d field position then false
+      else check_xmas field (go position direction) direction tl
+
+let check_mas field position direction =
+  match
+    ( get_2d field @@ go position direction,
+      get_2d field @@ go position @@ reverse direction )
+  with
+  | Some 'M', Some 'S' | Some 'S', Some 'M' -> true
+  | _ -> false
+
+let check_x_mas field position =
+  List.for_all (check_mas field position) diagonals
+
+let positions input =
+  let height = Array.length input in
+  let width = String.length input.(0) in
+  Enum.cartesian_product
+    (Enum.range 0 ~until:(width - 1))
+    (Enum.range 0 ~until:(height - 1))
+  |> Enum.map pos_from_pair
+
+let part1 input =
+  positions input
+  |> Enum.cartesian_product (all_directions ())
+  |> Enum.map (fun (dir, pos) -> check_xmas input pos dir xmas)
+  |> Enum.filter identity |> Enum.hard_count
+
+let part2 input =
+  positions input
+  |> Enum.filter (fun pos -> get_2d input pos = Some 'A')
+  |> Enum.map (check_x_mas input)
+  |> Enum.filter identity |> Enum.hard_count
+
+let () =
+  let input = get_input |> Array.of_list in
+  part1 input |> print_anything;
+  part2 input |> print_anything
--- lib/functional_helpers.ml
+++ lib/functional_helpers.ml
@@ -2,3 +2,17 @@ open Batteries
 
 let filter_some l = BatList.filter_map identity l
 let back_cons l e = l @ [ e ]
+
+module String = struct
+  include Stdlib.String
+  let get_opt container index =
+    if index < 0 || index >= length container then None
+    else Some (get container index)
+end
+
+module Array = struct
+  include Stdlib.Array
+  let get_opt container index =
+    if index < 0 || index >= length container then None
+    else Some (get container index)
+end
